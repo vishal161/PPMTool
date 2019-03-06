c7ff532de1356eedeb891d0bd7b01b4ff3f4df4e
vishal161 <48139606+vishal161@users.noreply.github.com>
2019-03-05 16:22:57 -0800

    Update ProjectController.java

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin
 public class ProjectController {
 
     @Autowired
