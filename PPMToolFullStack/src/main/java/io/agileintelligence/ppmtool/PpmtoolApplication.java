79b57cde256597a1908dd16d2f152c4654940de4
Vishal Nathan <Vishal.Nathan2@T-Mobile.com>
2019-05-02 13:56:12 -0700

    changes final
    changes final

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
@@ -2,9 +2,10 @@ package io.agileintelligence.ppmtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
+@EnableEurekaClient
 @SpringBootApplication
 public class PpmtoolApplication {
 
